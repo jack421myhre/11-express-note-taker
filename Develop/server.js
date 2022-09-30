d3bf8eb3490f0663779237c32f61cabc426da902
Jackson Myhre <jack421myhre@gmail.com>
2022-09-29 20:46:40 -0500

    Moved files around to properly read data. Added GET routes for homepage, /notes, /api/notes. Read and appended the new note to the database and repopulated the list to the page.

---
+++ Develop/server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const path = require("path");
+const { readAndAppend } = require("./helpers/fsUtils");
+
+const uuid = require("./helpers/uuid");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const refreshDataOnSave = () => {
+    app.get("/api/notes", (req, res) => {
+        res.sendFile(path.join(__dirname, "/db/db.json"));
+    });
+};
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(express.static("public"));
+
+// GET Route for homepage
+app.get("/", (req, res) =>
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.get("/notes", (req, res) =>
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+// api post and get
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.post("/api/notes", (req, res) => {
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        };
+
+        readAndAppend(newNote, "./db/db.json");
+        refreshDataOnSave();
+        const response = {
+            status: "SUCCESS",
+            body: newNote,
+        };
+        res.json(response);
+    } else {
+        res.json("ERROR");
+    }
+});
+
+// Get Everything
+app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
